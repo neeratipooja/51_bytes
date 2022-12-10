e9338a1ffeee6a2b891dc2145f6df86635d58e51
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-12-10 21:59:38 +0530

    Add files via upload

---
+++ bytes.py
@@ -0,0 +1,30 @@
+'''
+DATE:09TH NOV 2022
+DAY: FRIDAY
+TOPIC: BYTES
+AUTHOR: RAMA BHARGAVI
+'''
+# y=b"python"
+# print(y,type(y))
+# print(list(y))
+# print(tuple(y))
+# print(set(y))
+# y=b"python"
+# for i in y:
+#     print(i,end='  ')
+# print(ord( 'y'))
+# print(chr(80))
+# y=b"python"
+# print(type(y))
+# v=b'[1,2,3]'
+#print(list(v))
+print(ord('['))
+print(ord(','))
+print(chr(80))
+m=bytearray(b"python")
+print(m,type(m))
+print(m[1])
+m[0]=80
+print(m)
+
+
