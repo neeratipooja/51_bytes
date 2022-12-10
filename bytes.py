32a3ead870fbddf1463ad5bae2de461ae4932d23
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-12-10 22:02:34 +0530

    Update bytes.py

--- bytes.py
+++ bytes.py
@@ -4,20 +4,20 @@ DAY: FRIDAY
 TOPIC: BYTES
 AUTHOR: RAMA BHARGAVI
 '''
-# y=b"python"
-# print(y,type(y))
-# print(list(y))
-# print(tuple(y))
-# print(set(y))
-# y=b"python"
-# for i in y:
-#     print(i,end='  ')
-# print(ord( 'y'))
-# print(chr(80))
-# y=b"python"
-# print(type(y))
-# v=b'[1,2,3]'
-#print(list(v))
+y=b"python"
+print(y,type(y))
+print(list(y))
+print(tuple(y))
+print(set(y))
+y=b"python"
+for i in y:
+    print(i,end='  ')
+print(ord( 'y'))
+print(chr(80))
+y=b"python"
+print(type(y))
+v=b'[1,2,3]'
+print(list(v))
 print(ord('['))
 print(ord(','))
 print(chr(80))
